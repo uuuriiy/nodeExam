dcc21bedf3bc40e1b21e1df949aba543c7e33c63
yihnatchuk <yihnatchuk@griddynamics.com>
2022-09-09 19:09:40 +0300

    node exam done

---
+++ db.js
@@ -0,0 +1,28 @@
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url = 'mongodb://0.0.0.0:27017/';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'Users';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  db.collection('users');
+  db.collection('exercises');
+
+  // the following code examples can be pasted here...
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
